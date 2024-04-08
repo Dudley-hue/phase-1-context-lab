5bd46af5d4ba0701afed582dfa21fe913eb7ac18
dudley-hue <dudleykimani331@gmail.com>
2024-04-08 08:12:13 +0300

    first commit

--- index.js
+++ index.js
@@ -1,23 +1,89 @@
-/* Your Code Here */
+function parseTimestamp(timestamp) {
+    const [date, time] = timestamp.split(' '); // Sectionise date and time parts
+    const [year, month, day] = date.split('-').map(Number); // Parse the date parts as numbers
+    const hour = Number(time) / 100; //converts time to hours
+    return { year, month, day, hour };
+  }
+  
+  function createEmployeeRecord(array) {
+    return {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [4],
+        timeOutEvents: [5]
+    };
+  }
+  
+  function createEmployeeRecords(arrayOfArrays) {
+    return arrayOfArrays.map(createEmployeeRecord);
+  }
+  
+  // createTimeInEvent function
+  function createTimeInEvent(employeeRecord, dateTimeString) {
+    const [date, hour] = dateTimeString.split(' ');
+    const timeInEvent = {
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date: date
+    };
+    employeeRecord.timeInEvents.push(timeInEvent);
+    return employeeRecord;
+  }
+  
+  // createTimeOutEvent function
+  function createTimeOutEvent(employeeRecord, dateTimeString) {
+    const [date, hour] = dateTimeString.split(' ');
+    const timeOutEvent = {
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date: date
+    };
+    employeeRecord.timeOutEvents.push(timeOutEvent);
+    return employeeRecord;
+  }
+  
+  // Export the functions for use in other modules
+  module.exports = {
+    createTimeInEvent,
+    createTimeOutEvent
+  };
+  
+  //  hoursWorkedOnDate function
+  function hoursWorkedOnDate(employeeRecord, date) {
+    const timeInEvent = employeeRecord.timeInEvents.find(event => event.date === date);
+    const timeOutEvent = employeeRecord.timeOutEvents.find(event => event.date === date);
+    const hoursWorked = (timeOutEvent.hour - timeInEvent.hour) / 100;
+    return hoursWorked;
+  }
+  
+  //  wagesEarnedOnDate function
+  function wagesEarnedOnDate(employeeRecord, date) {
+    const hoursWorked = hoursWorkedOnDate(employeeRecord, date);
+    const wagesEarned = hoursWorked * employeeRecord.payPerHour;
+    return wagesEarned;
+  }
+  
+  // allWagesFor function
+  function allWagesFor(employeeRecord) {
+    const datesWorked = employeeRecord.timeInEvents.map(event => event.date);
+    const totalWages = datesWorked.reduce((acc, date) => acc + wagesEarnedOnDate(employeeRecord, date), 0);
+    return totalWages;
+  }
+  
+  //  calculatePayroll function
+  function calculatePayroll(employeeRecords) {
+    const totalPayroll = employeeRecords.reduce((acc, employeeRecord) => acc + allWagesFor(employeeRecord), 0);
+    return totalPayroll;
+  }
+  
+  // Export the functions for use in other modules
+  module.exports = {
+    hoursWorkedOnDate,
+    wagesEarnedOnDate,
+    allWagesFor,
+    calculatePayroll
+  };
 
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
-
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
-
-const allWagesFor = function () {
-    const eligibleDates = this.timeInEvents.map(function (e) {
-        return e.date
-    })
-
-    const payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
-
-    return payable
-}
 
